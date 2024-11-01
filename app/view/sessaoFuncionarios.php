99643a1a4405287bf4762420d12171c39e0d0e59
Jessybr <jessicabuenoramos5@gmail.com>
2024-10-31 21:28:18 -0300

    🚚 edit title

--- app/view/sessaoFuncionarios.php
+++ app/view/sessaoFuncionarios.php
@@ -27,7 +27,7 @@ if(isset($_GET['exclFunc'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Funcionários</title>
+    <title>Funcionários</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style/CabecalhoRodape.css">
@@ -84,7 +84,7 @@ if(isset($_GET['exclFunc'])) {
                                         <button id="excl"><a href="'.$redirectToEditar.'">Editar</a></button>        
                                         <button id="edit"><a href="'.$redirectToExcluir.'">Excluir</a></button>        
                                     </div>
-                                    </div>';
+                                </div>';
                         }
                     ?>
 
