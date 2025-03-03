9ffbf7c4c3bede96a808c3139afebbc17ce92350
jessyBr <jessicabuenoramos5@gmail.com>
2025-03-03 20:06:11 -0300

    ✨ add autoload

--- app/view/components/header.php
+++ app/view/components/header.php
@@ -1,3 +1,5 @@
+<?php 
+require_once '../../vendor/autoload.php'; ?>
 <header>
     <div class="conteiner-fluid d-flex justify-content-around cabecalho align-items-center">
         <div class="logo">
