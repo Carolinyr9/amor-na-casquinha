38c89084920c58a0cbd59dc707d173d115095eb6
jessyBr <jessicabuenoramos5@gmail.com>
2025-03-03 22:07:58 -0300

    🚚 rename file

--- app/model/ProdutoPedido.php
+++ app/model/ProdutoPedido.php
@@ -1,8 +1,8 @@
 <?php 
     require_once '../config/DataBase.php';
 
-    class OrderProduct{
-        private $idPedidoProduto;
+    class ProdutoPedido{
+        private $idProdutoPedido;
         private $idProduto;
         private $idPedido;
         private $variacao;
