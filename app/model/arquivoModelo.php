bb64399bfc620c3f6075fb8f4273c1435bb830e4
Bowie <carolinyr9@gmail.com>
2025-03-18 20:17:11 -0300

    ✅✨ creating models and tests

---
+++ app/model/arquivoModelo.php
@@ -0,0 +1,15 @@
+<?php
+class ArquivoModelo {
+    public function colocarNomeAcao(){
+        try {
+            //code...
+        } catch (PDOException) {
+            
+            //se for uma de consulta
+            //return objeto
+            // se for uma de alteracao
+           // return boleano
+        }
+    }
+}
+?>
