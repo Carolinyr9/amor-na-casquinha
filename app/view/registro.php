2e869d68364666f06fc8b76195561961fcc79dd0
Jéssica <134964436+Jessybr@users.noreply.github.com>
2025-05-03 19:35:05 -0300

    ♻️💄 refactor style in Registro page

--- app/view/registro.php
+++ app/view/registro.php
@@ -30,28 +30,74 @@ if(isset($_POST['resgistrar'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="style/CabecalhoRodape.css">
-    <link rel="stylesheet" href="style/loginS.css">
+    <link rel="stylesheet" href="style/components/botao.css">
+    <link rel="stylesheet" href="style/base/global.css">
+    <link rel="stylesheet" href="style/base/variables.css">
+    <link rel="stylesheet" href="style/registro.css">
 </head>
 <body>
     <?php
         include_once 'components/header.php';
     ?>
     <main>
-    <h3 class="m-auto fw-bold text-center">Login</h3>
+    <h3 class="subtitulo">Login</h3>
     <div class="d-flex justify-content-center align-items-center">
-        <form action="registro.php" method="post" class="d-flex flex-column my-4 formu">
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="nome" maxlength="50" placeholder="Nome" pattern="^[A-Za-zÀ-ÿ\s]+$" title="O nome deve conter apenas letras e espaços" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="email" name="email" maxlength="60" placeholder="Email" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="tel" name="celular" maxlength="25" placeholder="(XX) XXXXX-XXXX" pattern="^\(\d{2}\) \d{5}-\d{4}$" title="Coloque no formato (XX) XXXXX-XXXX" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="cep" maxlength="20" placeholder="CEP" pattern="^\d{5}-?\d{3}$" title="Coloque no formato 99999-999"  required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="rua" maxlength="100" placeholder="Rua" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="bairro" maxlength="45" placeholder="Bairro" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="number" name="numero" placeholder="Número" min="1" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="complemento" maxlength="15" placeholder="Complemento">
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="cidade" maxlength="45" placeholder="Cidade" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="text" name="estado" maxlength="45" placeholder="Estado" required>
-            <input class="rounded-4 border-0 fw-bold pl-3" type="password" name="senha" maxlength="255" placeholder="Senha" minlength="8" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}" title="A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma minúscula, um número e um caractere especial." required>
-            <input class="rounded-4 border-0 fw-bold m-auto" type="submit" value="Registre-se" name="resgistrar" class="btn btn-primary mt-3">
+        <form action="registro.php" method="post" class="d-flex flex-row flex-wrap justify-content-center align-items-center gap-4 rounded-4 p-4">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" class="form-control" name="nome" placeholder="Nome" pattern="^[A-Za-zÀ-ÿ\s]+$"  title="O nome deve conter apenas letras e espaços" maxlength="50" required>
+            </div>
+
+            <div class="form-group">
+                <label for="email">E-mail:</label>
+                <input type="email" class="form-control" name="email" placeholder="E-mail" maxlength="60" required>
+            </div>
+
+                <div class="form-group">
+                <label for="celular">Celular:</label>
+                <input type="tel" class="form-control" name="celular" maxlength="25" placeholder="(XX) XXXXX-XXXX" pattern="^\(\d{2}\) \d{5}-\d{4}$" title="oque no formato (XX) XXXXX-XXXX" required>
+            </div>
+
+            <div class="form-group">
+                <label for="rua">Rua:</label>
+                <input type="text" class="form-control" name="rua" maxlength="100" placeholder="Rua" required>
+            </div>
+            
+            <div class="form-group">
+                <label for="numero">Número:</label>
+                <input type="number" class="form-control" name="numero" placeholder="Número" min="1" required>
+            </div>
+
+            <div class="form-group">
+                <label for="complemento">Complemento:</label>
+                <input type="text" class="form-control" name="complemento" maxlength="15" placeholder="Complemento">
+            </div>
+
+            <div class="form-group">
+                <label for="cep">CEP:</label>
+                <input type="text" class="form-control" name="cep" maxlength="20" placeholder="CEP" pattern="^\d{5}-?\d{3}$" title="oque no formato 99999-999"  required>
+            </div>
+
+            <div class="form-group">
+                <label for="bairro">Bairro:</label>
+                <input type="text" class="form-control" name="bairro" maxlength="45" placeholder="Bairro" required>
+            </div>
+
+            <div class="form-group">
+                <label for="cidade">Cidade:</label>
+                <input type="text" class="form-control" name="cidade" maxlength="45" placeholder="Cidade" required>
+            </div>
+
+            <div class="form-group">
+                <label for="estado">Estado:</label>
+                <input type="text" class="form-control" name="estado" maxlength="45" placeholder="Estado" required>
+            </div>
+
+            <div class="form-group">
+                <label for="senha">Senha:</label>
+                <input type="password" class="form-control" name="senha" maxlength="255" placeholder="Senha" minlength="8" pattern="(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}" title="A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma minúscula, um número e um caractere especial." required>
+            </div>
+            <div class="form-group d-flex align-items-center justify-content-center w-100 "><input class="btn btn-primary" type="submit" value="Registre-se" name="resgistrar" class="btn btn-primary mt-3"></div>
         </form>
 </div>
     </main>
