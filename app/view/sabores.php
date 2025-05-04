41d428c990a9c7628cda355a344b43a41fc091c8
Jéssica <134964436+Jessybr@users.noreply.github.com>
2025-05-03 21:57:52 -0300

    ♻️💄 refactor style in Sabores page

--- app/view/sabores.php
+++ app/view/sabores.php
@@ -11,33 +11,37 @@ $produtos = $categoriaId ? $produtoController->selecionarProdutosAtivos($categor
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
+<head>5
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sabores</title>
     <link rel="stylesheet" href="style/CabecalhoRodape.css">
-    <link rel="stylesheet" href="style/saboresS.css">
-    <link rel="shortcut icon" href="images/iceCreamIcon.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style/components/botao.css">
+    <link rel="stylesheet" href="style/components/cards.css">
+    <link rel="stylesheet" href="style/base/global.css">
+    <link rel="stylesheet" href="style/base/variables.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">  
 </head>
 <body>
 <main>
-    <h1 class="m-auto text-center pt-4 pb-4">Sabores</h1>
-    <div class="container d-flex flex-column align-items-center justify-content-center">
-        <div class="container1 d-flex flex-row flex-wrap justify-content-center">
-            <?php
-                if (is_array($produtos) && count($produtos) > 0) {
-                    foreach ($produtos as $produto) {
-                        include 'components/produtosCards.php';
-                    }
-                } else {
-                    echo '<p>Nenhum produto encontrado.</p>';
-                }
-            ?>
+    <section class="container-md">
+        <h1 class="titulo">Sabores</h1>
+        <div class="conteiner container-md d-flex flex-column justify-content-center align-items-center rounded-4 p-4 m-md-0">
+                <div class="d-flex flex-md-column flex-row flex-wrap gap-4 justify-content-center align-items-center">
+                    <?php
+                        if (is_array($produtos) && count($produtos) > 0) {
+                            foreach ($produtos as $produto) {
+                                include 'components/produtosCards.php';
+                            }
+                        } else {
+                            echo '<p>Nenhum produto encontrado.</p>';
+                        }
+                    ?>
+                </div>
+        <a class="botao botao-secondary wx-auto mt-5" href="index.php">Voltar</a>
         </div>
-        <button class="voltar d-flex justify-content-center align-items-center fs-4 fw-bold rounded-4 mt-5 border-0">
-            <a class="text-decoration-none fs-5" href="index.php">Voltar</a>
-        </button>
-    </div>
+    </section>
 </main>
 <?php include_once 'components/footer.php'; ?>
 </body>
