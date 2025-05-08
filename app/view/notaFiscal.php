0448ac6b8cdec8e7a023e57b8d0d29a4c463d1e2
Jéssica <134964436+Jessybr@users.noreply.github.com>
2025-05-07 21:58:02 -0300

    ♻️💄 refactor notaFiscal style page

--- app/view/notaFiscal.php
+++ app/view/notaFiscal.php
@@ -11,30 +11,33 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="style/CabecalhoRodape.css">
     <link rel="stylesheet" href="style/notaFiscalS.css">
+    <link rel="stylesheet" href="style/base/variables.css">
+    <link rel="stylesheet" href="style/base/global.css">
+    <link rel="stylesheet" href="style/components/botao.css">
     <link rel="shortcut icon" href="images/iceCreamIcon.ico" type="image/x-icon">
 </head>
 <body>
     <?php include_once 'components/header.php'; ?>
     <main>
-        <div class="conteiner1 container d-flex flex-column align-items-center rounded-4">
-            <h3 class="mt-3">Confirmar Pedido?</h3>
-
+        <h1 class="titulo">Confirmar Pedido?</h1>
+        <div class="container-pedido container d-flex flex-column justify-content-center align-items-center rounded-4 w-50 p-4" style="background-color: var(--quaternary);">
             <?php if (isset($_SESSION["userEmail"])): ?>
-                <form id="pedidoForm" name="pedidoForm" method="post" action="perfil.php">
-                    <input type="hidden" name="isDelivery" id="isDelivery" value="<?= $isDelivery ? '1' : '0' ?>">
-
-                    <input name="ckbIsDelivery" id="ckbIsDelivery" type="checkbox" value="1" <?= $isDelivery ? 'checked' : '' ?>>
-                    <label for="ckbIsDelivery" id="labelForCkbIsDelivery">
-                        O pedido será entregue no seu endereço!
-                    </label>
+                <form id="pedidoForm" name="pedidoForm" method="post" action="perfil.php" class="d-flex flex-column justify-content-center align-items-center">
+                    <div>
+                        <input type="hidden" name="isDelivery" id="isDelivery" value="<?= $isDelivery ? '1' : '0' ?>">
+                        
+                        <label for="ckbIsDelivery" id="labelForCkbIsDelivery" class="text-wrap"><input name="ckbIsDelivery" id="ckbIsDelivery" type="checkbox" value="1" <?= $isDelivery ? 'checked' : '' ?>>
+                            O pedido será entregue no seu endereço!
+                        </label>
+                    </div>
                     <?php include 'components/enderecoCard.php'; ?>
                     
-                    <div class="total-div">
+                    <div class="total-div d-flex flex-row align-items-center justify-content-between w-50 mt-3">
                         <h4>Subtotal</h4>
                         <p>R$ <?= number_format($subtotal, 2, ',', '.') ?></p>
                     </div>
 
-                    <div class="frete-div" id="freteDiv" style="display: <?= is_numeric($frete) ? 'block' : 'none' ?>;">
+                    <div class="frete-div flex-row align-items-center justify-content-between w-50" id="freteDiv" style="display: <?= is_numeric($frete) ? 'flex' : 'none' ?>;">
                         <h4>Frete</h4>
                         <p>
                             <?php if (is_numeric($frete)): ?>
@@ -46,7 +49,7 @@
                         <input type="hidden" name="frete" id="frete" value="<?= htmlspecialchars($frete); ?>">
                     </div>
 
-                    <div class="total-com-frete">
+                    <div class="total-com-frete d-flex flex-row align-items-center justify-content-between w-50 mb-3">
                         <h4>Total do Pedido</h4>
                         <p>R$ <?= number_format($total, 2, ',', '.') ?></p>
                         <input type="hidden" name="totalComFrete" id="totalComFrete" value="<?= htmlspecialchars($total); ?>">
@@ -75,10 +78,10 @@
                     <input type="hidden" name="idEndereco" value="<?= htmlspecialchars($endereco->getIdEndereco()); ?>">
 
                     <input type="hidden" name="notaFiscal" value="1">
-                    <input name="btnSubmit" id="btnSubmit" type="submit" value="Concluir Pedido" class="btnConcluir border-0 px-3 mb-3 rounded-4">
+                    <input name="btnSubmit" id="btnSubmit" type="submit" value="Concluir Pedido" class="botao botao-primary my-4">
                 </form>
             <?php else: ?>
-                <button id="btnGoToLogin" class="btnGoToLogin w-75 text-wrap h-auto rounded-3 mb-3 border-0 p-2" onclick="window.location.href='login.php'">Faça Login para Concluir seu Pedido</button>
+                <button id="btnGoToLogin" class="link" onclick="window.location.href='login.php'">Faça Login para Concluir seu Pedido</button>
             <?php endif; ?>
         </div>
     </main>
