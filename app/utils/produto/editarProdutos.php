ff954b0e7b06c024eaa5d5e3d6d639c4b449377d
Jéssica <134964436+Jessybr@users.noreply.github.com>
2025-05-26 19:59:47 -0300

    ♻️ refactor editarProdutos.php to improve image handling logic

--- app/utils/produto/editarProdutos.php
+++ app/utils/produto/editarProdutos.php
@@ -4,12 +4,19 @@ use app\utils\helpers\Logger;
 require_once __DIR__ . '/../helpers/fotoHandler.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnEditar'])) {
+    
+    if(isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+        $foto = fotoHandler();
+    } else {
+        $foto = $_POST['imagemSabEdt'];
+    }
+
     $dados = [
         'id' => $_POST['idProduto'] ?? '',
         'nome' => $_POST['nomeProdEdt'] ?? '',
         'marca' => $_POST['marcaProdEdt'] ?? '',
         'preco'=> $_POST['precoSabEdt'] ?? '',
-        'foto' => fotoHandler(),
+        'foto' => $foto,
     ];
 
     $idProduto = $_POST['idProduto'] ?? '';
