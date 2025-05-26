b26b66a14af93ccc9b005013be3a390791639828
Jéssica <134964436+Jessybr@users.noreply.github.com>
2025-05-26 19:58:55 -0300

    ♻️ refactor editarCategorias.php to handle image upload more robustly

--- app/utils/categoria/editarCategorias.php
+++ app/utils/categoria/editarCategorias.php
@@ -6,7 +6,13 @@ require_once __DIR__ . '/../helpers/fotoHandler.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['btnEditar'])) {
 
     $id = $_GET['categoria'] ?? null;
-    $foto = fotoHandler();
+
+    if(isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK) {
+        $foto = fotoHandler();
+    } else {
+        $foto = $_POST['imagemProdEdt'];
+    }
+
     $dados = [
         'id' => $id,
         'nome' => $_POST['nomeProdEdt'] ?? '',
