b3033306bd46985192e4925f82b87988337c2515
Jéssica <134964436+Jessybr@users.noreply.github.com>
2025-06-05 20:14:48 -0300

    :sparkles: add styleLinks.php to include Bootstrap, jQuery, and custom stylesheets

---
+++ app/utils/links/styleLinks.php
@@ -0,0 +1,8 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="style/base/global.css">
+<link rel="stylesheet" href="style/base/variables.css">
+<link rel="stylesheet" href="style/CabecalhoRodape.css" />
+<link rel="stylesheet" href="style/components/botao.css">
+<link rel="stylesheet" href="style/components/cards.css">
+<link rel="shortcut icon" href="../images/iceCreamIcon.ico" type="image/x-icon">
